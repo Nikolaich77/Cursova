8607565641d61410eb2e9e47b87fa34053c40980
Nikolaich77 <kolya.havryluik@gmail.com>
2023-11-03 00:34:49 +0200

    front pizdec

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ document.addEventListener("DOMContentLoaded", function () {
     const todoList = document.getElementById("todoList");
     const doingList = document.getElementById("doingList");
     const doneList = document.getElementById("doneList");
-    const closeSidebarButton = document.querySelector(".close-button");
 
     addButton.addEventListener("click", addTask);
     taskInput.addEventListener("keyup", function (event) {
@@ -19,11 +18,13 @@ document.addEventListener("DOMContentLoaded", function () {
             const li = document.createElement("li");
             li.innerHTML = `
                 <span>${taskText}</span>
+                <button class="edit-button">Edit</button>
                 <button class="remove-button">Remove</button>
             `;
             todoList.appendChild(li);
             taskInput.value = "";
             addRemoveListener(li);
+            addEditListener(li);
             li.setAttribute("draggable", true);
             li.setAttribute("id", "task-" + new Date().getTime());
             li.addEventListener("dragstart", drag);
@@ -37,45 +38,44 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
 
-    function createEditForm(initialText, initialImportance, taskElement) {
+    function addEditListener(li) {
+        const editButton = li.querySelector(".edit-button");
+        editButton.addEventListener("click", function () {
+            const taskText = li.querySelector("span").textContent;
+            const textColor = getComputedStyle(li.querySelector("span")).color; // Отримати поточний колір тексту
+            createEditForm(taskText, textColor, li);
+        });
+    }
+
+    function createEditForm(initialText, textColor, taskElement) {
         const editForm = document.createElement("div");
         editForm.innerHTML = `
             <input type="text" id="editTaskInput" value="${initialText}">
-            <label for="importance">Важливість:</label>
-            <select id="importance" name="importance">
-                <option value="low">Низька</option>
-                <option value="medium">Середня</option>
-                <option value="high">Висока</option>
-            </select>
-            <button id="saveButton">Зберегти</button>
+            <input type="color" id="taskColor" value="${textColor}" list="colorOptions">
+            <datalist id="colorOptions">
+                <option>#ff0000</option> <!-- Червоний -->
+                <option>#ffff00</option> <!-- Жовтий -->
+                <option>#00ff00</option> <!-- Зелений -->
+            </datalist>
+            <button id="saveButton">Save</button>
         `;
-        const importanceSelect = editForm.querySelector("#importance");
-        importanceSelect.value = initialImportance;
 
         const saveButton = editForm.querySelector("#saveButton");
         saveButton.addEventListener("click", function () {
             const editedText = editForm.querySelector("#editTaskInput").value;
-            const editedImportance = editForm.querySelector("#importance").value;
+            const editedTextColor = editForm.querySelector("#taskColor").value;
 
             if (editedText.trim() !== "") {
-                const updatedTask = document.createElement("li");
-                updatedTask.innerHTML = `
-                    <span class="task-text">${editedText}</span>
-                    <button class="edit-button">Edit</button>
-                    <button class="remove-button">Remove</button>
-                `;
-                updatedTask.setAttribute("data-importance", editedImportance);
-                todoList.replaceChild(updatedTask, taskElement);
-                addRemoveListener(updatedTask);
-                addEditListener(updatedTask);
-                updatedTask.setAttribute("draggable", true);
-                updatedTask.setAttribute("id", taskElement.getAttribute("id"));
-                editForm.remove();
+                taskElement.querySelector("span").textContent = editedText;
+                taskElement.querySelector("span").style.color = editedTextColor; // Змінити колір тексту
+                editForm.style.display = "none";
             }
         });
 
         document.querySelector(".container").insertBefore(editForm, document.querySelector(".columns"));
     }
+
+    // ... інші функції та обробники
 
     function drag(event) {
         event.dataTransfer.setData("text", event.target.id);
@@ -104,11 +104,3 @@ function drop(event) {
         }
     }
 }
-
-// Функція для відкриття бічної панелі
-function openSidebar() {
-    const sidebar = document.getElementById("sidebar");
-    sidebar.style.display = "block";
-}
-
-closeSidebarButton.addEventListener("click", closeSidebar);
